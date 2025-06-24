0129fbd357d428d1d8eef52a1e64fc2646249078
Hektor Sun <21b908031@stu.hit.edu.cn>
2025-06-24 18:47:16 +0800

    build(SMC): 重新编译项目并更新代码
    - 添加了多个新的源文件，包括 stm32h7xx_hal_dma2d.c、stm32h7xx_hal_cryp_ex.c 等
    - 移除了部分冗余的源文件，如 led.c 和 Fan.c
    - 优化了代码结构，调整了部分源文件的编译顺序
    - 修复了多个编译警告，包括无效的多字节字符序列和未引用的变量
    - 更新了软件包和组件文件的版本

--- USER/main.c
+++ USER/main.c
@@ -14,14 +14,6 @@
 static volatile uint32_t report_tick = 0;
 const uint32_t REPORT_INTERVAL = 1000; // 100Hz下为10s
 
-// 调试用
-float duty_cycle_1[12] = {50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0};
-float duty_cycle_2[12] = {10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 5.0, 95.0};
-float duty_cycle_3[12] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0};
-float duty_cycle_4[12] = {15.5, 45.2, 75.8, 32.1, 87.4, 52.5, 19.0, 68.3, 90.9, 22.7, 40.6, 60.5};
-float duty_cycle_5[12] = {99.9, 0.1, 50.5, 25.5, 75.3, 10.0, 90.0, 5.0, 80.0, 20.0, 100.0, 0.0};
-// 调试用
-
 int main(void)
 {
     Cache_Enable();                 // 打开L1-Cache
@@ -34,18 +26,18 @@ int main(void)
     // u16 times = 0;     // 计时
 
 #if !OPERATING_MODE
-    USART_SendFormatted(&TERM_UART, "\r\n[DEBUG-MODE] 调试模式已启用: UART1 收+发\r\n");
+    USART_SendFormatted(&TERM_UART, "\r\n[DEBUG-MODE] UART1 send & receive\r\n");
 #else
-    USART_SendFormatted(&TERM_UART, "\r\n[OPERATING] 运行模式已启用: UART2 收, UART1 调试输出\r\n");
+    USART_SendFormatted(&TERM_UART, "\r\n[OPERATING-MODE] UART2 receive data, UART1 debug output\r\n");
 #endif
 #endif
 
 #if ENABLE_TIMER
 #if !OPERATING_MODE
-    USART_SendFormatted(&TERM_UART, "\r\n[DEBUG-MODE] 调试模式已启用: 以串口接收中断响应调试数据帧, 非固定频率\r\n");
+    USART_SendFormatted(&TERM_UART, "\r\n[DEBUG-MODE] Debug data frames responded via UART1, with non-fixed frequency.\r\n");
 #else
     TIM6_Init(); // 初始化TIM6
-    USART_SendFormatted(&TERM_UART, "\r\n[OPERATING] 运行模式已启用: 以100Hz频率执行控制响应(或其他频率, 具体请查看定时器头文件)\r\n");
+    USART_SendFormatted(&TERM_UART, "\r\n[OPERATING-MODE] Control responses are executed at a frequency of 100Hz (or other frequencies, please refer to the timer header file).\r\n");
 #endif
     PWM_Init(); // PWM初始化
     Set_Fan_PWM(&Fan_Control_duty_rate);
@@ -59,7 +51,7 @@ int main(void)
     extern FanSpeed Fan_desire_Speed;
     extern FanControl Fan_Control_duty_rate;
 
-    USART_SendFormatted(&TERM_UART, "\r\n控制器初始化\r\n");
+    USART_SendFormatted(&TERM_UART, "\r\nController Initialized\r\n");
 
     // 初始化参数并初始化控制器
     InitConstantsToFloat();                    // 初始化常量为浮点数
